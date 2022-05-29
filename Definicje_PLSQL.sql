9b401b31f8e984be362cecf46a6b97d2e576bc98
Hubert <hugo7744@gmail.com>
2022-05-29 18:35:34 +0200

    poprawiony trigger od ocen, procedura do wstawiania dyrektora

--- Definicje_PLSQL.sql
+++ Definicje_PLSQL.sql
@@ -1,3 +1,22 @@
+-- procedura, ktora dodaje nowego dyrektora
+CREATE OR REPLACE PROCEDURE nowy_dyrektor(n_id NUMBER)
+AS
+    poprzedni Dyrektorowie%rowtype;
+    data_rozp_poprz Dyrektorowie.data_rozpoczecia%type;
+    curr_date Dyrektorowie.data_rozpoczecia%type;
+    nowy_id  Dyrektorowie.dyrektor_id%type;
+BEGIN
+    SELECT MAX(data_rozpoczecia) INTO data_rozp_poprz FROM Dyrektorowie;
+    SELECT * INTO poprzedni FROM Dyrektorowie WHERE data_rozpoczecia = data_rozp_poprz;
+    IF poprzedni.nauczyciel_id = n_id THEN 
+        raise_application_error(-20003, 'Proba przypisania obecnego dyrektora jako nowego');
+    END IF;
+    SELECT SYSDATE INTO curr_date FROM dual;
+    UPDATE Dyrektorowie SET data_zakonczenia = curr_date WHERE dyrektor_id = poprzedni.dyrektor_id;
+    nowy_id := poprzedni.dyrektor_id + 1;
+    INSERT INTO Dyrektorowie VALUES (nowy_id, curr_date, null, n_id);
+END;
+
 -- funkcja ktora bierze id nauczyciela i zwraca liczbe pelnych lat (365 dni), ktore dany nauczyciel juz przepracowal w tej szkole
 CREATE OR REPLACE FUNCTION ile_lat_pracuje(n_id NUMBER)
 RETURN NUMBER
@@ -11,15 +30,15 @@ BEGIN
 END;
 /
 
--- trigger, ktory sprawdza czy dany nauczyciel moze byc dyrektorem (czy ma co najmniej 30 lat stazu)
+-- trigger, ktory sprawdza czy dany nauczyciel moze byc dyrektorem (czy ma co najmniej 15 lat stazu)
 CREATE OR REPLACE TRIGGER tr_czy_moze_byc_dyrektorem
 BEFORE INSERT OR UPDATE ON Dyrektorowie FOR EACH ROW
 DECLARE
     lata NUMBER;
 BEGIN
-    lata := ile_lat_pracuje(:old.nauczyciel_id);
-    IF lata < 30 THEN
-        raise_application_error(-20001, 'Dyrektor musi miec co najmniej 30 lat stazu');
+    lata := ile_lat_pracuje(:new.nauczyciel_id);
+    IF lata < 15 THEN
+        raise_application_error(-20001, 'Dyrektor musi miec co najmniej 15 lat stazu');
     END IF;
 END;
 /
@@ -30,9 +49,12 @@ DECLARE
     kl_id NUMBER;
     czy_jest NUMBER;
 BEGIN
-    SELECT klasa_id INTO kl_id FROM Konkretne_przedmioty WHERE konkretny_przedmiot_id = :old.konkretny_przedmiot_id;
-    SELECT COUNT(*) INTO czy_jest FROM Uczniowie WHERE uczen_id = :old.uczen_id AND klasa_id = kl_id;
-    IF czy_jest = 0 THEN
+    SELECT klasa_id INTO kl_id FROM Konkretne_przedmioty WHERE konkretny_przedmiot_id = :new.konkretny_przedmiot_id;
+    SELECT COUNT(*) INTO czy_jest FROM Uczniowie WHERE uczen_id = :new.uczen_id AND klasa_id = kl_id;
+    IF czy_jest < 1 THEN
         raise_application_error(-20002, 'Uczniowi zostala przypisana ocena z przedmiotu, na ktory nie chodzi');
     END IF;
 END;
+/
+
+
